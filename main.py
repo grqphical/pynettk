e62ce0cefd9d5fb3fabaa0dfb392d547c06cfc75
grqphical07 <95062977+grqphical07@users.noreply.github.com>
2022-09-15 11:04:51 -0700

    Added Status Code viewer

--- main.py
+++ main.py
@@ -30,6 +30,13 @@ def API_test(sender, data, user_data):
             response_data += f'{k} : {v}\n\n'
         
         dpg.configure_item("Response", default_value=response_data)
+        if request.status_code >= 200 and request.status_code < 300:
+            dpg.configure_item("StatusCode", default_value=request.status_code, color=[0,255,0])
+        elif request.status_code >= 400 and request.status_code < 600:
+            dpg.configure_item("StatusCode", default_value=request.status_code, color=[255,0,0])
+        else:
+            dpg.configure_item("StatusCode", default_value=request.status_code)
+        
 
 # Adds a parameter to the list box and the global array
 def add_parameter(sender, data, user_data):
@@ -64,6 +71,8 @@ with dpg.window(label="API Test", width=300, height=600, no_close=True):
     dpg.add_listbox(label="Method", items=methods, callback=set_method)
     dpg.add_button(label="Send", callback=API_test)
     dpg.add_text()
+    dpg.add_text("Status Code:")
+    dpg.add_text(tag="StatusCode")
     dpg.add_text("Response:")
     dpg.add_input_text(tag="Response", multiline=True, readonly=True, height=250, width=284)
 
